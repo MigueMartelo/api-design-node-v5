64d54a60cefd0b5987fd89de6694b7775d6b1a83
Miguel Martelo <miguemartelo1031@gmail.com>
2025-12-17 22:49:21 -0500

    Add authentication controller, routes, and JWT utilities

---
+++ src/controllers/authController.ts
@@ -0,0 +1,81 @@
+import type { Request, Response } from 'express';
+import bcrypt from 'bcrypt';
+import { db } from '../db/connection.ts';
+import { users } from '../db/schema.ts';
+import { generateToken } from '../utils/jwt.ts';
+import { comparePassword, hashPassword } from '../utils/passwords.ts';
+import { eq } from 'drizzle-orm';
+
+export const register = async (req: Request, res: Response) => {
+  try {
+    const hashedPassword = await hashPassword(req.body.password);
+
+    const [user] = await db.insert(users).values({
+      ...req.body,
+      password: hashedPassword,
+    })
+      .returning({
+        id: users.id,
+        email: users.email,
+        username: users.username,
+        firstName: users.firstName,
+        lastName: users.lastName,
+        createdAt: users.createdAt,
+      });
+
+    const token = await generateToken({
+      id: user.id,
+      email: user.email,
+      username: user.username
+    });
+
+    return res.status(201).json({
+      message: 'User created',
+      user,
+      token,
+    })
+  } catch (error) {
+    console.error('Registration error', error);
+    res.status(500).json({ error: 'Failed to create user'});
+  }
+}
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    const user = await db.query.users.findFirst({ where: eq(users.email, email) })
+
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid credentials!' })
+    }
+
+    const isValidPassword = await comparePassword(password, user.password);
+
+    if (!isValidPassword) {
+      return res.status(401).json({ error: 'Invalid credentials!' })
+    }
+
+    const token = await generateToken({
+      id: user.id,
+      email: user.email,
+      username: user.username
+    });
+
+    return res.status(201).json({
+      message: 'Login success',
+      user: {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        createdAt: user.createdAt,
+      },
+      token
+    });
+
+  } catch (error) {
+    console.error('Login error', error);
+    res.status(500).json({ error: 'Failed to login'});
+  }
+}

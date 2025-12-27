044f9e331ad53b1d9abe5e81da550c7d729b7067
Miguel Martelo <miguemartelo1031@gmail.com>
2025-12-27 12:06:55 -0500

    error handler

---
+++ src/middleware/errorHandlers.ts
@@ -0,0 +1,40 @@
+import type { NextFunction, Request, Response } from 'express';
+import env from '../../env.ts';
+
+class APIError extends Error {
+  status: number;
+  name: string;
+  message: string;
+
+  constructor(message: string, name: string, status: number) {
+    super()
+    this.message = message;
+    this.status = status;
+    this.name = name;
+  }
+}
+
+export const errorHandler = (err: APIError, req: Request, res: Response, next: NextFunction) => {
+  console.log(err.stack);
+
+  let status = err.status || 500;
+  let message = err.message || 'Internal server error';
+
+  if (err.name === 'ValidationError') {
+    status = 400;
+    message = 'Validation Error';
+  }
+
+  if (err.name === 'UnauthorizedError') {
+    status = 401;
+    message = 'Unauthorized'
+  }
+
+  return res.status(status).json({
+    error: message,
+    ...(env.APP_STAGE === 'dev' && {
+      stack: err.stack,
+      details: err.message
+    })
+  })
+}
--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import helmet from 'helmet';
 import cors from 'cors';
 import morgan from 'morgan';
 import { isTest } from '../env.ts';
+import { errorHandler } from './middleware/errorHandlers.ts';
 
 const app = express();
 
@@ -30,6 +31,8 @@ app.get('/health', (req, res) => {
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/habits', habitRoutes);
+
+app.use(errorHandler);
 
 export { app };
 
